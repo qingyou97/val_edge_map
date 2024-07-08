5edd977833ce6917a62b1ca71813c43c39e18438
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-08 14:52:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import os
-from PIL import Image
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
-def check_image_size(folder_path):
-    # 遍历文件夹中的所有文件
-    for filename in os.listdir(folder_path):
-        # 构造文件的完整路径
-        file_path = os.path.join(folder_path, filename)
-        try:
-            # 打开图像文件
-            with Image.open(file_path) as img:
-                # 检查图像尺寸是否为900x900
-                if img.size != (900, 900):
-                    print(f"{filename} 的尺寸不是900x900，而是 {img.size}")
-        except (IOError, SyntaxError) as e:
-            print(f"无法处理文件 {file_path}: {e}")
+writer = SummaryWriter(comment='test_tensorboard')
 
-# 设置要检查的文件夹路径
-folder_path = "A"
-check_image_size(folder_path)
+for x in range(100):
+    writer.add_scalar('y=2x', x * 2, x)
+    writer.add_scalar('y=pow(2, x)', 2 ** x, x)
+
+    writer.add_scalars('data/scalar_group', {"xsinx": x * np.sin(x),
+                                             "xcosx": x * np.cos(x),
+                                             "arctanx": np.arctan(x)}, x)
+writer.close()
