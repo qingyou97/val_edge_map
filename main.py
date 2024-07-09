aca27bae1cb821258869cbc55d52934e9f008846
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-09 17:35:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-import cv2
-import numpy as np
+import sys
+import cv2 as cv
+import os
 
-# 读取图像
-image = cv2.imread('your_image_path.jpg', cv2.IMREAD_GRAYSCALE) # 使用灰度模式读取图像
 
-# 检查图像是否成功加载
-if image is None:
-    print("图像加载失败！请检查文件路径。")
-    exit()
+def main(img_folder, save_path):
+     ddepth = cv.CV_16S
+     kernel_size = 3
 
-# 使用cv2.Laplacian检测图像边缘
-laplacian = cv2.Laplacian(image, cv2.CV_64F)
+     if not os.path.exists(save_path):
+          os.makedirs(save_path,exist_ok=True)
 
-# 将图像的深度转换回来以便显示在 0-255 范围内
-laplacian = np.uint8(np.absolute(laplacian))
+     img_list = os.listdir(img_folder)
 
-# 显示原始图像和Laplacian边缘检测结果
-cv2.imshow('Original Image', image)
-cv2.imshow('Laplacian Edge Detection', laplacian)
+     for img in img_list:
+          img_path = os.path.join(img_folder, img)
 
-# 等待用户关闭窗口
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+          src = cv.imread(img_path, cv.IMREAD_COLOR) # Load an image
+
+          if src is None:
+               print('Error opening image')
+               print('Program Arguments: [image_name -- default lena.jpg]')
+               return -1
+
+
+          # Remove noise by blurring with a Gaussian filter
+          src = cv.GaussianBlur(src, (3, 3), 0)
+
+          # Convert the image to grayscale
+          src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+          # Apply Laplace function
+          dst = cv.Laplacian(src_gray, ddepth, ksize=kernel_size)
+
+          # converting back to uint8
+          abs_dst = cv.convertScaleAbs(dst)
+          abs_dst = 255 - abs_dst
+          name = img.split('.')[0]
+          cv.imwrite(os.path.join(save_path, f'{name}.png'), abs_dst)
+     print('计算完成')
+     return 0
+if __name__ == "__main__":
+     img_folder = r'E:\DexiNed-master\data'
+     save_path = r'result/laplacian'
+     main(img_folder, save_path )
