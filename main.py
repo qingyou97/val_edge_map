a5d423c1d44dd466a39770d113a8a5ca864863e4
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-22 20:55:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-3. Writing a for-loop script to train data from 7 folders.
-4. Selected the first trained model and viewed the detection results of the baseline model for cable images. Currently believe there is no overfitting and the detection is quite good.
-5. Writing a for-loop script for cyclic pruning and inference in torch-pruning, adjusting parameters including importance method, p, quantity, and iteration times.
-6. Explanation of the torch-pruning method. Some methods still need to be detailed and further investigated.
-7. Created a follow-up plan in the afternoon.
+1JHCAI-64
+1.1 Had a meeting with you to explain the pruning methods in torch-pruning.
+Conclusion: Through code testing, metapruner can use four importance calculation methods: GroupNormImportance, BNScaleImportance, LAMPImportance, and FPGMImportance. Will continue to read today to understand what these four methods entail.
+
+2JHCAI-61
+2.1 Wrote a for-loop script to train data from 7 folders. Completed
+2.2 Selected the first training model and viewed the detection results of the baseline model on cable images.
+Conclusion: Currently believe there is no overfitting on cable images, and the detection results are pretty good. Completed
+2.3 Wrote a for-loop script for iterative pruning and inference in torch-pruning.
+Conclusion: The automatically adjusted parameters include the importance method (4 types), p (l1 norm or l2 norm), the number of pruned elements, and the number of iterations (how many iterations to complete this pruning). Pruning for cable has been completed; others need further testing.
+
+Scrum Manager
+3.1 Created subsequent plans in the afternoon. Completed
