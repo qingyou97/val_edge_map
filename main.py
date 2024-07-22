f5b57c66d30e176744a3e6a90b97b1a777fd9f5b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-22 13:32:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
- # 将预测结果乘以 255
-    fore_ground = fore_ground * 255
-    fore_ground = fore_ground.byte()  # 转换为字节格式
+Duan:
+1. Monday: Select multiple baseline models and check other datasets and defect types to see if there is overfitting.
+2. Tuesday and Wednesday: Use the torch-pruning method to prune the parameters of all selected baseline models according to the scope chosen by Li Zhe. Record all visualization results.
+3. Thursday: Compare all experiments of Li Zhe with all my own experiments, unify the format of deliverables, and organize my torch-pruning results. Compare with Li Zhe's experimental results.
+4. Friday: Jointly discuss the conclusions of pruning and generalization, and discuss the next steps, including explanations of the results with the official PyTorch pruning method.
 
-    # 选择第一张图像（如果存在多个批次）
-    fore_ground_img = fore_ground[0]
-
-    # 将 Tensor 转换为 NumPy 数组
-    fore_ground_img = fore_ground_img.numpy()
-
-    # 将 NumPy 数组转换为 PIL 图像
-    pil_img = Image.fromarray(fore_ground_img, mode='L')
-
-    # 保存灰度图像
-    pil_img.save('foreground.png')
+Li Zhe:
+5. Monday: Based on the experiments, select the results of the pruning scope.
+6. Tuesday and Wednesday: Use the official PyTorch pruning method to prune the parameters of all selected baseline models according to the chosen scope. Record all visualization results.
+7. Thursday: Organize my PyTorch official experiments according to Duan's format and compare with Duan's experimental results.
+8. Friday: Jointly discuss the conclusions of pruning and generalization, explain the results of the official PyTorch pruning method, and discuss the next steps.
