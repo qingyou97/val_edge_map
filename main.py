80b66bd9bfe79a910c8be7737da4702714b2a6c5
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-24 17:31:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
 import shutil
+import stat
 
 # 替换为你要清空的文件夹路径
 folder_path = "A"
 
+def remove_readonly(func, path, _):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
 for root, dirs, files in os.walk(folder_path):
     for file in files:
         file_path = os.path.join(root, file)
-        os.remove(file_path)
+        try:
+            os.remove(file_path)
+        except PermissionError:
+            os.chmod(file_path, stat.S_IWRITE)
+            os.remove(file_path)
     for dir in dirs:
         dir_path = os.path.join(root, dir)
-        shutil.rmtree(dir_path)
-        os.makedirs(dir_path)
+        try:
+            shutil.rmtree(dir_path, onerror=remove_readonly)
+            os.makedirs(dir_path)
+        except PermissionError:
+            os.chmod(dir_path, stat.S_IWRITE)
+            shutil.rmtree(dir_path, onerror=remove_readonly)
+            os.makedirs(dir_path)
