05c48651e51a3c8482b9be89f9a13609e4002bfa
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-24 17:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import os
+import shutil
 
-# A文件夹路径
-base_dir = 'A'
+# 定义A文件夹和B文件夹路径
+A_folder_path = "path/to/A_folder"
+B_folder_path = "path/to/B_folder"
 
-# 将在每个子文件夹下创建的四个新文件夹名称
-new_folders = ['test_gt', 'test_images', 'train_gt', 'train_images']
+# 定义函数来遍历文件夹中的所有文件夹和子文件夹
+for root, dirs, files in os.walk(A_folder_path):
+    for dir_name in dirs:
+        # 解析文件夹名称
+        item_name, sub_name = dir_name.rsplit('_', 1)
+        
+        # 分别获取A文件夹和B文件夹路径
+        src_folder = os.path.join(B_folder_path, item_name, "ground_truth", sub_name)
+        dest_folder = os.path.join(root, dir_name, "test_gt")
+        
+        # 确保目标文件夹存在，如果不存在则创建
+        os.makedirs(dest_folder, exist_ok=True)
 
-# 遍历A文件夹下的所有子文件夹
-for subdir in next(os.walk(base_dir))[1]:
-    subdir_path = os.path.join(base_dir, subdir)
-    for folder in new_folders:
-        new_folder_path = os.path.join(subdir_path, folder)
-        os.makedirs(new_folder_path, exist_ok=True)
+        # 获取源文件夹中的所有图像文件并复制到目标文件夹
+        if os.path.exists(src_folder):
+            for img_file in os.listdir(src_folder):
+                src_file_path = os.path.join(src_folder, img_file)
+                dest_file_path = os.path.join(dest_folder, img_file)
+                
+                if os.path.isfile(src_file_path):
+                    shutil.copy(src_file_path, dest_file_path)
+
+    # 因为只需要在第一层文件夹执行（内层递归不会改变Already_in的数量）
+    break
+
+print("完成人工分类操作")
