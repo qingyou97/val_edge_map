0bd75e7045b1f0fe6260c2bbc39c0d5996e15951
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-24 18:18:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import os
 
-def rename_images(base_folder):
-    # 遍历A文件夹下的所有子文件夹
-    for root, subdirs, files in os.walk(base_folder):
-        # 检查是否是"test_gt"文件夹
-        if os.path.basename(root) == "test_gt":
-            for file in files:
-                if "_mask" in file:  # 检查文件名是否包含"_mask"
-                    src = os.path.join(root, file)
-                    dst = os.path.join(root, file.replace('_mask', ''))
-                    os.rename(src, dst)
-                    print(f'Renamed: {src} -> {dst}')
+# 文件夹路径
+folder_path = 'A'  # 请将'A'替换为你的实际路径
 
-# 替换为你的文件夹路径
-base_folder = 'A文件夹路径'
-rename_images(base_folder)["bottle", "cable", "capsule", "carpet", "grid", "hazelnut", "leather", "metal_nut", "pill", "screw", "tile", "toothbrush", "transistor", "wood", "zipper"]
+# 获取所有文件夹名称
+subfolders = [f.name for f in os.scandir(folder_path) if f.is_dir()]
+
+print(subfolders)
