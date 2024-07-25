b7e4bb8c183c634aa866b44cf1fa029fe3e49758
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-25 20:10:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-1. I have completed the training of the remaining dataset, and Li Zhe has organized 6 types of training datasets.
-2. Organized four methods of importance measurement. Today I need to make a separate appointment to communicate with you.
-3. Filter the visualization results.
-The conclusion is that the pruning effects of the three baseline models indicate that there are cases where some anomalies cannot be detected by the baseline models but can be detected after pruning, showing an enhanced generalization ability. However, this results in a significant decrease in the detection ability for other images.
-
-Plan for today:
-1. Analyze the visualization results of the remaining part of the dataset to understand the relationship between generalization and pruning.
-2. Concurrently analyze the relationship between the pruning range and generalization.
+Reducing capacity
+Early stopping
+Regularization and weight decay
+Ensembles
+Data augmentation
+Stochastic regularization
