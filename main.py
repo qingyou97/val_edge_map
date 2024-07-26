9295852141f0a2bdf03500dcc73d3c40ef2d40e5
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-26 14:22:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-For the DCAMA open-source project, I completed the test data loading function based on its testing logic.
+For the DCAMA method, use the stored test data to complete the tests for five categories in fold0 and visualize the results. Organize the test results into the final results.
