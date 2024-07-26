83805d023e46b5baf01ce0128380318065b249fb
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-26 12:07:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-Looking for datasets for edge detection in industrial quality inspection.
-Seeking methods to accelerate training on CPU.
+Pros:
+Preliminary conclusions have been reached for generalizability tasks based on time requirements, and experiments have identified the relationship between generalizability and pruning.
+
+Cons:
+Sometimes the time allocation for scrum managers and technical roles is poorly managed, leading to excessive pressure to meet technical goals, which results in overtime work.
