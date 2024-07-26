61ac0c43c9aba45d5f4a941b82a82de22848112f
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-26 14:34:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-Based on 2.4, train DCAMA using bad query and mixed support. The data comes from fine-grained defect data from Industrial-5i.
+I standardized the format of deliverables and recorded my own experimental results while conducting torch-pruning experiments.
