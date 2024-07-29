b6cbd2e840eaf647512600f4fe9d14410dde820b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-29 09:07:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-All previous image results have been stored on the NAS, but the new results with modified parameters are being added due to their large number.
+Attended a meeting with Han Xu and Wang Chao and recorded the objectives.
