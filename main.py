3355b53b7554b6a1dee34b185e06baf80c95ea4b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-29 19:25:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-2. Performed inference using one of the datasets on BDCN.
-3. Created and organized tasks on Jira.
-4. Shared the temperature scaling method to enhance generalization for tasks.
-5. Assisted Li Zhe in aligning the task objectives.
+def is_image(file_path):
+    try:
+        Image.open(file_path).verify()
+        return True
+    except (IOError, SyntaxError):
+        return False
