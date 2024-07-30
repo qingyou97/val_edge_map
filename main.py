1eee3812da66036392463974da9e5269bce9fd55
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-30 20:12:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-1. Completed the planning and documentation for all subsequent experiments, totaling five. Created tickets in Jira, with descriptions and subtasks for each ticket completed.
-2. Conducted out-of-box tests on BDCN, pidinet, and dexined, and recorded the visualization results of seven datasets in a table.
-3. Searched for other edge detection datasets used for industrial inspection.
-a. Halcon belongs to the company MVTec.
-b. Among the six instance datasets in MVTec, three are segmentation datasets. None directly include edge GT datasets, but MVTecAD and MVTecLOCOAD might be indirectly converted to edge contours, though there will be a loss in accuracy. (The description of accuracy loss when converting from segmentation to edge detection appears in the CASENet paper, CASENet: Deep Category-Aware Semantic Edge Detection)
-c. Found six other segmentation datasets. One of these datasets deals with semantic defect segmentation on steel surfaces. After checking the GT, it seems suitable for creating edge detection GTs as it only marks the outer contours.
-d. Found four other detection datasets.
+1. Discuss whether the newly added semantic defect segmentation dataset of steel surfaces can be used as an edge detection dataset.
+2. Explore whether it is possible to convert segmentation datasets into edge detection datasets.
+3. Test the performance (accuracy, recall, and F-score) of the cracktree200 dataset on BDCN, PIDINET, and DEXINED.
