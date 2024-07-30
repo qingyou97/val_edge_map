892ecb1d5e43d088bbf4463304e8748f0fdd6604
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-30 19:20:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-没有直接带有边缘的gt数据集，但是MVTec D2S，MVTec AD ，MVTec LOCO AD也许可以间接转化成边缘轮廓，但会有精度损失（来自论文CASENet: Deep Category-Aware Semantic Edge Detection)
+1. Completed the planning and documentation for all subsequent experiments, totaling five. Created tickets in Jira, with descriptions and subtasks for each ticket completed.
+2. Conducted out-of-box tests on BDCN, pidinet, and dexined, and recorded the visualization results of seven datasets in a table.
+3. Searched for other edge detection datasets used for industrial inspection.
+a. Halcon belongs to the company MVTec.
+b. Among the six instance datasets in MVTec, three are segmentation datasets. None directly include edge GT datasets, but MVTecAD and MVTecLOCOAD might be indirectly converted to edge contours, though there will be a loss in accuracy. (The description of accuracy loss when converting from segmentation to edge detection appears in the CASENet paper, CASENet: Deep Category-Aware Semantic Edge Detection)
+c. Found six other segmentation datasets. One of these datasets deals with semantic defect segmentation on steel surfaces. After checking the GT, it seems suitable for creating edge detection GTs as it only marks the outer contours.
+d. Found four other detection datasets.
