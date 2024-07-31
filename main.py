72a06f97678f689b60a14dc05825cd16414d4914
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-31 09:05:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-BDCN performs better on the AIGLE_RN, crack200, ESAR, and datasets. Dexined performs better on the CFD dataset. Detection is not very good on the crack500, GAPS, and LCMS datasets.
+BDCN detects well on the AIGLE_RN, crack200, and ESAR datasets. Dexined performs better on CFD. Performance is not good on the crack500, GAPS, and LCMS datasets.
