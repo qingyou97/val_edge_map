3809915995e81b95f6fdf66d089c17f5080bf860
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-07-31 19:45:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-import os
-import cv2
-
-
-def make_edge_mask(seg_mask_path, edge_mask_path):
-    if not os.path.exists(edge_mask_path, ):
-        os.makedirs(edge_mask_path,exist_ok=True)
-
-    seg_mask_list = os.listdir(seg_mask_path)
-
-    for mask in seg_mask_list:
-        mask_path = os.path.join(seg_mask_path, mask)
-
-        # 读取mask图像
-        mask = cv2.imread(r'E:\seg2edge\20160222_080933_721_1921.png', 0)  # 灰度模式读取
-
-        # 使用Canny算法提取边缘
-        edges = cv2.Canny(mask, 100, 200)
-
-        # 保存或显示边缘图像
-        cv2.imwrite(os.path.join(edge_mask_path, mask), edges)
-        # cv2.imshow('Edges', edges)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-
-
-if __name__ == '__main__':
-    seg_mask_path = '' # 分割mask路径
-    edge_mask_path = '' # 边缘mask路径
-    make_edge_mask(seg_mask_path, edge_mask_path)
+1. I had a meeting and reported the datasets I found, and I understood your requirements for the datasets.
+2. According to the metal and engine directions, I found 11 datasets.
+Conclusion: Currently, I found 2 engine turbine blade datasets, 2 casting product image datasets, 4 industrial part datasets, 1 industrial product dataset from MVTec, 1 automobile part dataset, and 1 metal can dataset. Whether they meet the standards needs further discussion with you.
+Today's plan:
+1. Perform inference and organize visualization results of BDCN, pidinet, and dexined on the selected datasets.
