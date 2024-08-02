b40e4ead5d1d3f8afdafd0346c1d6f9b761d475d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-02 09:06:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-def resize_image(image, max_size):
-    _, _, H, W = image.shape
-    if H > max_size or W > max_size:
-        scaling_factor = max_size / float(max(H, W))
-        new_H = int(H * scaling_factor)
-        new_W = int(W * scaling_factor)
-        image = F.interpolate(image, size=(new_H, new_W), mode='bilinear', align_corners=False)
-    return image
+Comparison between five-shot and one-shot: test the middle one from five-shot with one-shot.
