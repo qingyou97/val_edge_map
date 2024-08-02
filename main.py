9634d914b73038ef2f20e841b8627b2ae4ee8081
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-02 09:04:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-1. Shared information about the datasets and discussed the final selected dataset.
-2. Reorganized 6 datasets, removed images without specified edges, kept the remaining images temporarily for testing purposes.
-3. Tested the 6 datasets on BDCN using a for loop, currently in progress.
-4. Prepared materials for team management and communicated team issues with you.
-5. Attended the meeting with Wang Chao and Han Xu, recorded optimization points.
-Today's plan:
-1. Conduct testing on pidinet and dexined.
-2. Organize visualization results.
-3. Look for edge annotation tools.
+def resize_image(image, max_size):
+    _, _, H, W = image.shape
+    if H > max_size or W > max_size:
+        scaling_factor = max_size / float(max(H, W))
+        new_H = int(H * scaling_factor)
+        new_W = int(W * scaling_factor)
+        image = F.interpolate(image, size=(new_H, new_W), mode='bilinear', align_corners=False)
+    return image
