72ca53896471215a29b1fb846aba5f75390e06a9
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-02 15:36:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import openpyxl
-
-# 加载 Excel 文件
-workbook = openpyxl.load_workbook("your_file.xlsx")
-
-# 检查是否存在名称为 "A" 的工作表
-if "A" in workbook.sheetnames:
-    workbook.remove(workbook["A"])
-
-# 保存修改后的文件
-workbook.save("your_file.xlsx")
+针对AERO数据集的飞机涡轮叶片，对于我们想要的边缘，dexined的检出能力更强，能检测出来很多我们需要的边缘。但是对于一些对比度较弱的边缘，还是检测的不多，但优于其他两个算法。
+对于casting-product数据集，对于我们想要的边缘，dexined的检出能力更强，且检测的最多，pidinet明显噪声较多，BDCN检测出我们需要的边缘较少。
+对于Industrial-Dataset下的round数据，对于我们想要的边缘，dexined的检出能力更强，且检测的最多，pidinet丢失了很多边缘，且检测的轮廓很粗。BDCN的检测边缘也粗，而且很多细节检测不到。
+对于Industrial-Dataset下的square数据，对于我们想要的边缘，dexined的检出能力更强，且检测的最多，pidinet丢失了很多边缘，且检测的轮廓很粗。BDCN的检测边缘也粗，而且很多细节检测不到。
+对于Industrial-Tools数据集下的ball-screw数据，对于我们想要的边缘，BDCN平衡了检出数量和噪声数量，pidinet的噪声过多，而dexined部分边缘并没有检测出来。
+对于Industrial-Tools数据集下的valve数据，对于我们想要的边缘，dexined平衡了检出数量和噪声数量，bdcn和pidinet的边缘很模糊，且检出数量不够。
+对于apple-company数据集下的Cylinder数据，对于我们想要的边缘，dexined平衡了检出数量和噪声数量，bdcn检出数量不够，pidinet有些地方很抽象。
+对于apple-company数据集下的Croove数据，对于我们想要的边缘，pidinet检出数量最多，而dexined检测的数量不够，bdcn噪声太多。
+对于apple-company数据集下的Ring数据，对于我们想要的边缘，dexined平衡了检出数量和噪声数量，pidinet的噪声也多，但bdcn噪声是最多的。
+对于mvtec的bottle类别，三者都不太好，dexined检出的数量已经算最多的了。
