a72a277dc27731d0584de21cc083d3dab560e94d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-06 17:35:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-from PIL import Image
-import numpy as np
-
-# 打开图像
-img = Image.open('你的图像文件路径.png').convert('L')
-
-# 将图像转换为Numpy数组
-pixel_values = np.array(img)
-
-# 统计每种像素值的数量
-unique, counts = np.unique(pixel_values, return_counts=True)
-
-# 将结果转换为字典
-pixel_count_dict = dict(zip(unique, counts))
-
-print("每种像素值的数量:")
-for pixel_value, count in pixel_count_dict.items():
-    print(f"像素值 {pixel_value}: {count} 个像素点")
+Here is my summary after reading the pidinet code and paper on reading gt. My conclusion is that the pidinet paper and code indeed set a threshold to discard some part of the gt. I used to think there were only pixel values of 0 and 255 in my png images converted from 7 people's annotated mat, but my code was actually problematic. This caused my use of the default 0.3 threshold in the project to discard pixel values of 51, resulting in incorrect training results. So, I re-experimented using a 0.1 threshold to ensure all pixels in my gt were preserved. I also updated the training results in the table. The results are different when training with gt annotated by 7 people versus gt annotated by 1 person.
+All the steps are documented in this Word file.
