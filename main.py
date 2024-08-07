df0dabbcf6b6af4a309f0d7401ad132f3cf3e064
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-07 18:07:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-1. What is the annotation tool used by DexiNed, and how to find Sun Zhen?
-2. Delete non-compliant images when doing the test with the remaining images. (For future training, take 20 for training)
-3. When training flowers with DexiNed, first, 70 epochs are too few. Try more epochs to see if it can fit. Second, don't let the learning rate drop; train directly with 0.001 and see the effect. 
-4. Train using an image on BDCN.
-5. After annotating the data, train with one image and test it.
+I just want to confirm if 5:30-6:00 counts as our working hours and if the time manager recorded this time as our working hours?
