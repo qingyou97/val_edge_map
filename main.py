0c6fff981ccb9b2ff0ad75152b2701608e90e3e4
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-07 11:17:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-I am currently using GIMP image processing software to annotate edge detection images. It is an open-source tool for image processing and composition. While it is not specifically designed for edge detection annotation, I believe it can be used for this purpose since I haven't found specialized edge annotation tools. I have documented every detailed step of using it in a Word document. Additionally, I have recorded other non-usable software I came across during my search.
+1. What is the annotation tool used by DexiNed, and how to find Sun Zhen?
+2. Delete non-compliant images when doing the test with the remaining images. (For future training, take 20 for training)
+3. When training flowers with DexiNed, first, 70 epochs are too few. Try more epochs to see if it can fit. Second, don't let the learning rate drop; train directly with 0.001 and see the effect. 
+4. Train using an image on BDCN.
+5. After annotating the data, train with one image and test it.
