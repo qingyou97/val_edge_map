1f1694c3d6e6b3119b3146e532f3d10a07cceb76
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-08 17:29:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-When I use pidinet to loop train on 20 images, I notice the third image has a size of 3000x4000, causing memory issues. GitHub mentions that pidinet cannot train on high-resolution images. I think resizing the image and gt to 512x512, similar to dexined preprocessing, might help. Do you think this is reasonable?
+self.scale_transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor()
+        ])
