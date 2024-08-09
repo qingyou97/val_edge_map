867bc7c9d9bd329fb052c1b315d78eea3a9a7368
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-09 19:55:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from torchvision import transforms
+import os
+from PIL import Image
 
-   transform = transforms.Compose([
-       transforms.Resize((512, 512)),  # 统一尺寸
-       transforms.ToTensor()
-   ])
+def resize_images(folder_path, output_folder, size=(512, 512)):
+    # 确保输出文件夹存在
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-   dataset = CustomDataset(transform=transform)
+    for filename in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, filename)
+        with Image.open(img_path) as img:
+            img = img.resize(size)
+            img.save(os.path.join(output_folder, filename))
+
+# 定义输入和输出文件夹
+a_folder = 'A文件夹路径'
+b_folder = 'B文件夹路径'
+a_output = 'A_output'
+b_output = 'B_output'
+
+# 调用函数处理A和B文件夹
+resize_images(a_folder, a_output)
+resize_images(b_folder, b_output)
