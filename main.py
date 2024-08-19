04b1c04dd2219fe63e14366a8a5fc88f5b83d3b3
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-19 08:56:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-import os
-from PIL import Image
-
-def resize_images(folder_path, output_folder, size=(512, 512)):
-    # 确保输出文件夹存在
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    for filename in os.listdir(folder_path):
-        img_path = os.path.join(folder_path, filename)
-        with Image.open(img_path) as img:
-            img = img.resize(size)
-            img.save(os.path.join(output_folder, filename))
-
-# 定义输入和输出文件夹
-a_folder = 'A文件夹路径'
-b_folder = 'B文件夹路径'
-a_output = 'A_output'
-b_output = 'B_output'
-
-# 调用函数处理A和B文件夹
-resize_images(a_folder, a_output)
-resize_images(b_folder, b_output)
+1. Deploy TEED, infer 10 objects in 6 datasets.
+2. New annotations for 41 images in 2 datasets.
+3. Pidinet for-loop training, for the new 2 datasets, training one image at a time per dataset.
+4. Pidinet training, using all 100 images in 5 datasets.
+5. Pidinet for-loop training, for 5 datasets, training 3, 5, or 7 random images each time per dataset.
+6. BDCN for-loop training, for 5 datasets, training one image at a time per dataset.
+7. BDCN training, using all 100 images in 5 datasets.
