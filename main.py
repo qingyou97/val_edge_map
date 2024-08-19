0db1efc13dba1fe779ccc6eba34662c912c0c831
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-19 09:42:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-1. Deploy TEED, infer 10 objects in 6 datasets.
-2. New annotations for 41 images in 2 datasets.
-3. Pidinet for-loop training, for the new 2 datasets, training one image at a time per dataset.
-4. Pidinet training, using all 100 images in 5 datasets.
-5. Pidinet for-loop training, for 5 datasets, training 3, 5, or 7 random images each time per dataset.
-6. BDCN for-loop training, for 5 datasets, training one image at a time per dataset.
-7. BDCN training, using all 100 images in 5 datasets.
+1. Organize and compare the results of TEED reasoning
+2. There are a total of 121 scenarios for PiDiNet. 100 models each trained on one image at a time, 1 model trained on all 100 images at once, 20 models trained on 3/5/7/9 images from 5 different datasets each time. Today, select 3 scenarios out of these 121, and make predictions and reasoning for 100, 200, and 299 epochs. Calculate recall, precision, and F1 score against GT (ground truth).
+3. BDCN trains on 3/5/7/9 images each time.
+4. PiDiNet trains on the BIPED dataset.
