29049476cca8570a01d53f564cecdb3e399b84f5
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-19 14:07:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-1. Organize and compare the results of TEED reasoning
-2. There are a total of 121 scenarios for PiDiNet. 100 models each trained on one image at a time, 1 model trained on all 100 images at once, 20 models trained on 3/5/7/9 images from 5 different datasets each time. Today, select 3 scenarios out of these 121, and make predictions and reasoning for 100, 200, and 299 epochs. Calculate recall, precision, and F1 score against GT (ground truth).
-3. BDCN trains on 3/5/7/9 images each time.
-4. PiDiNet trains on the BIPED dataset.
+Although TEED, as stated in the paper, detects finer edges, it loses many details. For some datasets where the other three algorithms perform well, TEED performs worse.
