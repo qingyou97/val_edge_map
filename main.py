7859056ded5ed0c8b8f25f9cdd48bc209d45b220
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-19 14:11:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-Although TEED, as stated in the paper, detects finer edges, it loses many details. For some datasets where the other three algorithms perform well, TEED performs worse.
+Algorithms: BDCN, PIDINET, DEXINED
+Training methods:
+1. Train one image at a time
+2. Train 3, 5, 7, or 9 images at a time
+3. Train on the entire dataset
+4. Additionally train PIDINET on the BIPED dataset
