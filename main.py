f4ca7346c098379e1d96ae910a0ba5ea3639cf1b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-20 17:27:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 def write_image_paths_to_txt(folder_path, output_file):
-    with open(output_file, 'w') as file:
+    with open(output_file, 'w', newline='\
+') as file:
         for root, dirs, files in os.walk(folder_path):
             for name in files:
                 if name.lower().endswith(('png', 'jpg', 'jpeg', 'bmp', 'gif')):
                     file.write(os.path.join(root, name) + '\
 ')
-
-# 使用示例
-folder_path = 'A'  # 你的文件夹路径
-output_file = 'output.txt'  # 输出的txt文件名
-write_image_paths_to_txt(folder_path, output_file)
