4b6144d14889d941ea36b52797123b37b9b4918f
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-20 14:52:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,15 @@ def resize_by_half(image):
     height, width = image.shape[:2]
     height //= 2
     width //= 2
-    
-    # 形态学操作：膨胀和腐蚀
-    kernel = np.ones((3, 3), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    image = cv2.erode(image, kernel, iterations=1)
-    
-    # 使用最近邻插值法
-    image = cv2.resize(image, (width, height), interpolation=cv2.INTER_NEAREST)
-    return image
+    resized_image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LINEAR) # 使用双线性插值
+    return resized_image
 
 if __name__ == "__main__":
-    image_path = r"D:\\edge_detection\\code\\datasets\\metal_engine_data\\reorganization\\Metal_Training_original\\1-Aero-engine-defect\\gt_binary\\4.png"
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image_path = r"D:\\edge_detection\\code\\datasets\\metal_engine_data\\reorganization\\Metal_Training_original\\1-Aero-engine-defect\\gt_binary/4.png"
+    image = cv2.imread(image_path, 0)  # 0 表示读取为灰度图像
+    resized_image = resize_by_half(image)
     
-    if image is not None:
-        resized_image = resize_by_half(image)
-        cv2.imwrite("resized_image_cv2.jpg", resized_image)
-    else:
-        print("无法读取图片")
+    # 进行二值化处理
+    _, binary_resized_image = cv2.threshold(resized_image, 127, 255, cv2.THRESH_BINARY) 
+
+    cv2.imwrite("resized_image_cv2.jpg", binary_resized_image)
