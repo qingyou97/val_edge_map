07d02cfca88188ca2a3c10ca51bf676139b645bb
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-20 11:39:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-1.1 Label the remaining 40 datasets.
-2.1 Train BDCN and test results and metrics.
-2.2 Train PiDiNet and test results and metrics.
-2.3 Train DexiNed and test results and metrics.
-3.1 Test metrics and visualize comparisons for four scenarios of each dataset (using different amounts of data for training models).
-3.2 Determine if there is a common pattern in each dataset.
-4.1 Test and rank the metrics for each model.
-4.2 Compare and analyze the results to determine if there is a common pattern in each dataset.
-5.1 Learn the augmentation methods for the BIPEDv2 dataset.
-5.2 Train PiDiNet.
-5.3 Test the results and analyze.
+from PIL import Image
+
+# 转换pyl浮数组为image对象
+image = Image.fromarray(lb.astype(np.uint8))
+
+# 保存为图像文件
+image.save('output_image.png')
