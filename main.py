0be6876d0244064292dc9f5fd4c3bcee48e684f9
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-21 09:21:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def write_image_paths_to_txt(folder_path, output_file):
-    with open(output_file, 'w', newline='\
-') as file:
-        for root, dirs, files in os.walk(folder_path):
-            for name in files:
-                if name.lower().endswith(('png', 'jpg', 'jpeg', 'bmp', 'gif')):
-                    file.write(os.path.join(root, name) + '\
-')
+1. Confirm interpolation method after resizing to see if resizing thickens edge and causes gt edge loss.
+Conclusion: Checked my code, default is bicubic interpolation, which thickens edges. PIDInet code previously set threshold to 0.1, filtering out some values. Lowered PIDInet threshold to prevent this issue. Switched to nearest neighbor interpolation, which keeps edge thickness at 1 but causes discontinuity in edges. Decided to stick with bicubic interpolation (`order=3`), re-binarized greyscale values through post-processing.
+2. Re-trained PIDInet and BDCN with images scaled to 512x512. Trials include training with a single image, and randomly with 2/3/5/7/9/11 images, and all 100 images. PIDInet re-trained on the BIPEDv2 dataset.
+Conclusion: As of this morning, PIDInet has trained extensively on the large BIPED dataset; only two small metal datasets remain. Similarly, BDCN also only has two small metal datasets left.
+3. Finished training scripts for three settings in dexined: training with a single image, 2/3/5/7/9/11 images, and all 100 images to see different effects.
+Conclusion: This morning, checked Wang Chao's machine. Dexined nearly done training the first dataset. Suppose dexined performs inference and auto-saves model in the same path after each model training, slowing things down.
+
+Today's Plan:
+4. Forecast results and metrics for bdcn and pidinet.
+5. TEED inference under three cases: training with 1 image, training with 2/3/5/7/9/11 images, and training all 100 images.
+6. Study how TEED fusion module is improved.
