dfa271f2d8e5dee2f3b61ba37792df7825f91fc6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-23 15:48:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def generate_excel_columns(start_column='AD'):
     columns = []
-    start_ord = ord(start_column[0]) * 26 + ord(start_column[1]) - 16384
+    start_ord = (ord(start_column[0]) - ord('A') + 1) * 26 + ord(start_column[1]) - ord('A')
     
     for i in range(start_ord, 16384):  # 16384 corresponds to 'XFD' column
         if i < 26:
@@ -9,5 +9,3 @@ def generate_excel_columns(start_column='AD'):
             columns.append(chr(i // 26 - 1 + ord('A')) + chr(i % 26 + ord('A')))
     
     return columns
-
-columns = generate_excel_columns(start_column='AD')
