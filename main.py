5ee610d4275e07f0c2679ab3d40e72bf3cea25dc
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-26 14:29:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-Using bdcn results as an example,
-In 5 categories of datasets, for images with small inter-class distance like 4-ball-screw, one image can be well-fitted.
-For images with large inter-class distance, like 1-Aero-engine-defect and 2-casting-product-image, if one image is trained, it detects well for similar images.
-But for other images, detection performance is poor.
-All results of bdcn experiments are recorded in tables.
+Research methods to accelerate training in fine-tuning.
