583fdf61f8e3b876dce84f22b63da8ddea0996a4
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-26 13:40:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-5 datasets, train on one image at a time; (analysis finished)
-5 datasets, train on all images at once; (analysis finished)
-5 datasets, randomly train on 3/5/7/9 images at a time; (analysis finished)
-5 datasets, each time add the image with the lowest f1 score for training (not yet started)
+Here is my current progress table for the four network experiments. Please review it.
