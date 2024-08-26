c15758bb5429eae216b60ab52be5be32659561c6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-26 13:31:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-Red indicates that the training script has been written and is currently running or has already completed the training.
+5 datasets, train on one image at a time; (analysis finished)
+5 datasets, train on all images at once; (analysis finished)
+5 datasets, randomly train on 3/5/7/9 images at a time; (analysis finished)
+5 datasets, each time add the image with the lowest f1 score for training (not yet started)
