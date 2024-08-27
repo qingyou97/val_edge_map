a2d1699a4bde7bb14caf58840173857819e7e423
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-27 20:13:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def set_parameter_requires_grad(model):
-    for name, param in model.named_parameters():
-        if not re.match(r'classifier.*', name):
-            param.requires_grad = False
-        else:
-            param.requires_grad = True
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+        print(x.shape)
+        return x
