99dca74224298618379c3aaf2cd67e03a6b7eb24
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-28 20:21:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-class FeatureExtractor(nn.Module):
-    def __init__(self, num_classes):
-        super(FeatureExtractor, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU()
-        
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
-        
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
+# 去掉前缀 'features.'
+new_state_dict = {}
+for k in state_dict:
+    new_key = k.replace('features.', '')  # 去掉前缀
+    new_state_dict[new_key] = state_dict[k]
 
-    def forward(self, x):
-        x = self.relu1(self.bn1(self.conv1(x)))
-        x = self.relu2(self.bn2(self.conv2(x)))
-        x = self.conv3(x)
-        # print(x.shape)
-        return x
+# 加载修改后的 state_dict
+model_class.load_state_dict(new_state_dict)
