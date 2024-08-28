8f2538813728f322b89dead6ea78fc65086aa446
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-28 20:44:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# 去掉前缀 'features.'
-new_state_dict = {}
-for k in state_dict:
-    new_key = k.replace('features.', '')  # 去掉前缀
-    new_state_dict[new_key] = state_dict[k]
+Regarding the implementation of the classification layer added after BDCN, the following has been completed and the code uploaded to GitLab:
 
-# 加载修改后的 state_dict
-model_class.load_state_dict(new_state_dict)
+1. Added a simple classification layer, code in classifier.py.
+2. Solved the gradient back-propagation issue; loss now decreases normally. The cause was modified SGD parameters. Haven't yet investigated why passing weight_decay and momentum led to constant loss.
+3. Moved BDCN fuse process and data loading outside the training loop, time is reduced.
+4. Changed classification to three categories, including background as the third category.
+5. Changed the loss function to CrossEntropyLoss.
+6. Currently, inference code is not complete. Training on 20 casting images is ongoing. On Tuesday, I will write the test code and test the model.
