da741ca098ad48a6dd9a5bab8fa4402587cdc631
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-28 11:00:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-1. Summary of various aspects of the training of the four networks.
-Conclusion: Currently, I have compared the three trained networks from 6 aspects to determine which dataset quantity yields the best results. Yesterday, the teed training was completed, but I haven't run inference and analysis yet, so I conducted the comparison and analysis of the other 6 aspects.
-
-2. In bdcn, add a classification layer at the final fusion layer to suppress unwanted edges.
-Conclusion: A classification layer has been added, and ground truth (gt) is correctly input. Positive samples are input gt, and negative samples are edges fused and subtracted from gt. Following pidinet, the fused background is first labeled as 0.5, then the mask is set to 0, so the background is excluded from the calculation. Currently, the loss does not change in each training epoch, and saving the model hasn't been implemented yet. I am looking for the problem.
+1. Verify the influence of distance on pidinet, change from l2 distance to cosine distance.
+2. Create a list of experiments.
+3. Reproduce the results of dexined.
