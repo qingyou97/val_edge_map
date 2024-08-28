59553954f94f4bccaf665d804afd98d3fb3b042d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-08-28 18:53:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-
 class FeatureExtractor(nn.Module):
     def __init__(self, num_classes):
         super(FeatureExtractor, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU()
-
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
-
-
-        # nn.init.constant_(self.conv1.weight, 10)
-        # nn.init.constant_(self.conv1.bias, 1)
-        #
-        # nn.init.constant_(self.conv2.weight, 15)
-        # nn.init.constant_(self.conv2.bias, 4)
+        
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+        
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
-        x = self.relu1(self.conv1(x))
-        x = self.conv2(x)
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.conv3(x)
         # print(x.shape)
         return x
