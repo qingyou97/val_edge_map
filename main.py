75c5b4473e4010a18948a4d175f6b26cdb7cfe51
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 17:54:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ class FeatureExtractor(nn.Module):
         super(FeatureExtractor, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU()
-        
+        self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
-
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(num_classes)
-        
+        self.shortcut = nn.Sequential()
+
     def forward(self, x):
-        x = self.relu1(self.bn1(self.conv1(x)))
-        x = self.relu2(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
-        return x
+        residual = x
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(residual)
+        return out
