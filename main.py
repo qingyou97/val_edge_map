79ec7fb8453620f0b71ec65f298311a9b57a2920
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 09:01:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-1. Comparison with out-of-box: whether illumination causes performance degradation
-
-Conclusion:
-
-① Casting dataset: Images labeled with a yellow background have detection performance degraded due to illumination effects. Images with a pinkish background have poor detection due to the object's outer contour being jagged. Images with a green background are extremely affected by alternating light and dark due to illumination.
-
-② Cylinder dataset: The reflective surface of the object leads to non-continuous edges. This situation is common.
-
-③ Groove dataset: The lowest horizontal stripe is highly reflective and very bright, causing the two parallel lines at the bottom to fail detection. Images with a pinkish background indicate low light, causing low contrast at the bottom edge and thus failing detection.
-
-2. BDCN fine-tuning and generalization improvement
-
-Conclusion: The dropout method significantly solves the problem of discontinuous edges and greatly improves generalization.
-
-① Sheet1 includes experiments with distinctive and representative results. For example, when using the SGD optimizer: although unwanted edges were greatly suppressed, the internal contours became extremely discontinuous, suppressing many desired edges.
-
-② After switching to the Adam optimizer: fitting improved, internal contours remained discontinuous, but the results were much better than those with SGD, even though many desired edges were still suppressed.
-
-③ Increasing the learning rate: resulted in retaining more desired edges but also introduced issues with unwanted edges appearing.
-
-④ Seeking solutions: The dropout method was found. Adding dropout after each ReLU and setting it to 0.5 and other parameters significantly solved the discontinuity problem of edges, greatly improving generalization. Many shallow edges could now detect both contours, but some unwanted edges still remained.
+网络很浅且训练图像只有1张的话，会导致训练结果的随机性很大。这样的条件不足以提供足够的数据来让模型有效学习，因此结果可能会非常不稳定和不准确。
+If the network is very shallow and the training image set contains only one image, it will lead to high randomness in the training results. Such conditions are insufficient to provide enough data for the model to learn effectively, and thus the results may be very unstable and inaccurate.
