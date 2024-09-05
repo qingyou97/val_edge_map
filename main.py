0cd947c51a655fee0b9999380f659315baf815d6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 13:06:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-Morning Meeting 0905 10:30-11:05:
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-Duan:
-1. bdcn's generalization on a single image can be improved. Consider adjusting parameters or model size without learning, or adjusting pidinet to suppress edge pixels.
-2. The new classifier added to bdcn shows promise in suppressing unwanted parts, but needs more improvement. Ensure it works very well on the trained image. If the classifier is added at the final fusion layer, distance relationships may not be used. Consider adding the classifier at a shallower block. Evaluate this.
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_classes):
+        super(FeatureExtractor, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=num_classes, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(num_classes),
+            nn.ReLU(),
+        )
 
-Han:
-3. Fix one support for testing to compare results with Duan's.
-4. Improve generalization (patch core) and suppress unwanted edges, especially in the AERO dataset.
-5. Train dcama only on the metal dataset. Make necessary adjustments and send to Teams, priority higher than point 2.
-6. Train dcama using only pidinet.
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
