cbe5dc7ec972aab14f8445aeadf6a42fb76d01c3
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 09:56:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# 查看gt
-# new = torch.squeeze(new_label, dim=0)
-# print(f'new shape:{new.shape}')
-# check_label = torch.zeros_like(new)
-# check_label[new == 1] = 1
-# check_label = check_label.numpy()
-# cv2.imwrite('label1.png', 255 * check_label)
-#
-# check_label1 = torch.zeros_like(new)
-# check_label1[new == 0] = 1
-# check_label1 = check_label1.numpy()
-# cv2.imwrite('label0.png', 255 * check_label1)
-#
-# check_label2 = torch.zeros_like(new)
-# check_label2[new == 2] = 1
-# check_label2 = check_label2.numpy()
-# cv2.imwrite('label2.png', 255 * check_label2)
+# 获取并排序文件夹名
+folder_names = os.listdir(folder_path)
+folder_names.sort(key=natural_sort_key)
