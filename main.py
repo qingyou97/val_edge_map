5296ccdc2a8fa4a510407e1c22162941ef60f5a0
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 13:37:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-# 获取并排序文件夹名，并过滤掉不包含'_result'的文件夹
-folder_names = [name for name in os.listdir(folder_path) if '_result' in name]
-folder_names.sort(key=natural_sort_key)
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_classes):
+        super(FeatureExtractor, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=num_classes, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(num_classes),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
