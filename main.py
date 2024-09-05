0865444109266d61587383646ad0a2403dc0bf9b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 13:41:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
 class FeatureExtractor(nn.Module):
     def __init__(self, num_classes):
         super(FeatureExtractor, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=num_classes, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(num_classes),
-            nn.ReLU(),
-        )
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+        
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
 
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(num_classes)
+        
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
         return x
