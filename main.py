301c445ae31a6fe527b58ce794a5f7b96efa9054
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 11:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-3.Determine the feasibility of fine-tuning the BDCN classifier this week.
-Conclusion: After correcting some errors in the training network, we tried various parameter settings, such as batch size and larger models, but found it easy to encounter overfitting. Later, I carefully adjusted the label weights according to the method of pidinet and set the epoch to 300. We found that near 300 epochs, there is some fitting to the ground truth. However, edges tend to be discontinuous. Currently, the shallow network and having only 1 training image result in significant randomness, making the outcomes very unstable and inaccurate. I provided an example in the table.
+Morning Meeting 0905 10:30-11:05:
+
+Duan:
+1. bdcn's generalization on a single image can be improved. Consider adjusting parameters or model size without learning, or adjusting pidinet to suppress edge pixels.
+2. The new classifier added to bdcn shows promise in suppressing unwanted parts, but needs more improvement. Ensure it works very well on the trained image. If the classifier is added at the final fusion layer, distance relationships may not be used. Consider adding the classifier at a shallower block. Evaluate this.
+
+Han:
+3. Fix one support for testing to compare results with Duan's.
+4. Improve generalization (patch core) and suppress unwanted edges, especially in the AERO dataset.
+5. Train dcama only on the metal dataset. Make necessary adjustments and send to Teams, priority higher than point 2.
+6. Train dcama using only pidinet.
