f747d885b844d638c239ee813aeea8600f6eb714
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-05 18:55:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-class SimpleCNN(nn.Module):
-    def __init__(self, in_channels=3, num_classes=3):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 512 * 512, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-        self.relu = nn.ReLU()
-        
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_classes=3):
+        super(FeatureExtractor, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
+    
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+        x = self.conv3(x)
         return x
