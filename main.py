eac388617b1e86c730e0af2887e9e1877ad4558f
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-06 10:58:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-1. Improving the generalization of training one image with BDCN
-   Conclusion:
-   a. I have fine-tuned the dropout in BDCN; currently, there is one result that is better at suppression than before.
-   b. I input this result into the classifier and suppressed the internal edges again. I found that the classifier is effective, and many previously shallow edges were suppressed. So, I think my classifier and logic are temporarily fine. But every time I add the classifier to the fuse, the effect is very unstable and difficult to fit.
+0906 Morning Meeting 10:00-10:50:
 
-2. The scheme of connecting the classifier after the fuse
-   Conclusion:
-   a. The scheme of connecting the classifier after the fuse was later changed to directly connecting the classifier with the deep2shallow3 block. After adding the classifier with deep2shallow3 block and fuse concatenated, I found that the effect is very unstable and hard to control.
-   b. For the scheme of connecting the classifier after the fuse, sometimes fine-tuning a few times, fixing the random seed, and lowering the learning rate can occasionally achieve effects like in lines 12 and 13. But currently, there are still intermittent internal edges and cases where suppression does not occur.
+Duan:
+1. Apply the plan to improve the generalization of one-image training in BDCN to other works and see the effect.
+2. Change the classifier to binary classification. Previous results were not good, possibly due to loss conflict. Align classifications with ground truth this time.
+3. Try the previous three-class classification on other works to verify consistency and check if the suppression effect or loss conflict was the issue.
+4. Try segmenting first, using SEG module, then add the one-shot approach; this may become a new plan.
+5. Learn and implement Optuna in the future, but note that implementation will take time; consider it for later use.
+
+Han Xu: 
+6. Change the backbone in patch core and check the results.
+7. Assign the PC with Duan, run experiments over the weekend.
+8. Improve the one-shot plan further to completion and make a summary.
+9. Run 3, 4, 5 experiments:
+  a. Try image augmentation of support.
+  b. Use different upstream blocks in Pidinet.
+  c. Adjust weights for each backbone block.
+10. Train DCAMA with 100 samples to see if it's a training issue.
