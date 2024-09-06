534ad85df806d58f323c17c0bde15f7afca86b6a
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-06 13:35:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-Login is fine now, Han Sang currently has permission to access Wang Chao's machine.
+# 找到 f1 最大的那一组
+max_f1_group = max(result_dict.values(), key=lambda x: x['f1'])
+
+print(max_f1_group)
