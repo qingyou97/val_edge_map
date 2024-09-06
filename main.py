d649fcd333cde73967365ac40edc9c4e18273074
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-06 17:26:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,24 @@
-def test_with_ori(bdcn_model, class_model, data_root, res_dir, thresh = 0.5):
-    mean_bgr = np.array([104.00699, 116.66877, 122.67892])
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_classes=3):
+        super(FeatureExtractor, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
 
-    test_root = data_root
+        )
 
-    test_lst = os.listdir(test_root)
+        self.decoder = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 1, kernel_size=3, padding=1),
+            nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
+        )
 
-    save_dir = res_dir
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-
-    bdcn_model.eval()
-    class_model.eval()
-
-    start_time = time.time()
-    all_t = 0
-
-
-    for nm in test_lst:
-        data = cv2.imread(test_root + '/' + nm)
-
-        print(f'Processing data: {nm}')
-
-        data = np.array(data, np.float32)
-        data -= mean_bgr
-        data = data.transpose((2, 0, 1))
-        data = torch.from_numpy(data).float().unsqueeze(0)
-
-        data = Variable(data)
-
-        t1 = time.time()
-        out = bdcn_model(data)
-
-        if '/' in nm:
-            nm = nm.split('/')[-1]
-
-        fused_map = out[-1].cpu().data
-
-        out = [F.sigmoid(out[-1]).cpu().data.numpy()]
-        save_out = [out[-1][0, 0, :, :]]
-
-        fused_map = Variable(torch.tensor(data))
-        feature_map = class_model(fused_map)
-
-
-        class_map = F.sigmoid(feature_map).cpu().data.numpy()[0, 0, :, :]
-
-        fused_map = fused_map.numpy()[0, 0, :, :]
-        new_map = np.zeros_like(fused_map)
-        new_map[(class_map>=thresh) & (fused_map>=thresh)] = 1
-
-        # 保存原模型的fused map
-        if not os.path.exists(os.path.join(save_dir, 'fuse')):
-            os.mkdir(os.path.join(save_dir, 'fuse'))
-        cv2.imwrite(os.path.join(save_dir, 'fuse/%s.png' % nm.split('/')[-1].split('.')[0]), 255 - 255 * save_out[-1])
-
-        # 保存经过分类之后的模型
-        if not os.path.exists(os.path.join(save_dir, 'class_1')):
-            os.mkdir(os.path.join(save_dir, 'class_1'))
-
-        cv2.imwrite(os.path.join(save_dir, 'class_1/%s.png' % nm.split('/')[-1].split('.')[0]),
-                    255- 255 * new_map)
-        if not os.path.exists(os.path.join(save_dir, 'image')):
-            os.mkdir(os.path.join(save_dir, 'image'))
-
-        cv2.imwrite(os.path.join(save_dir, 'image/%s.png' % nm.split('/')[-1].split('.')[0]),
-                    255- 255 * class_map)
-
-        all_t += time.time() - t1
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
