c185e06c3414ad57e6c5ab1e1ef8fa7f41215a4d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-09 14:42:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import torch
-import math
-
 def get_sinusoidal_position_encoding(batch_size, h, w, d_model, device):
     pe = torch.zeros(h, w, d_model, device=device)
-    y_position = torch.arange(0, h, dtype=torch.float, device=device).unsqueeze(1)
-    x_position = torch.arange(0, w, dtype=torch.float, device=device).unsqueeze(0)
 
-    div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float, device=device) * 
+    y_position = torch.arange(0, h, dtype=torch.float, device=device).unsqueeze(1).repeat(1, w)
+    x_position = torch.arange(0, w, dtype=torch.float, device=device).unsqueeze(0).repeat(h, 1)
+
+    div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float, device=device) *
                          -(math.log(10000.0) / d_model))
 
-    pe[:, :, 0::2] = torch.sin(y_position * div_term).unsqueeze(1)
-    pe[:, :, 1::2] = torch.cos(x_position * div_term).unsqueeze(0)
+    pe[:, :, 0::2] = torch.sin(y_position.unsqueeze(-1) * div_term)
+    pe[:, :, 1::2] = torch.cos(x_position.unsqueeze(-1) * div_term)
 
     pe = pe.unsqueeze(0).expand(batch_size, -1, -1, -1)  # (B, H, W, D)
     return pe.permute(0, 3, 1, 2)  # (B, D, H, W)
