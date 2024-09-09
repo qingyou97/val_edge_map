9e166345bd701b13d60d6ba8ac65bcbab304f81d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-09 15:49:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
- self.data_transforms = transforms.Compose([
-                    transforms.RandomHorizontalFlip(),
-                    transforms.RandomRotation(15),
-                    transforms.RandomResizedCrop((self.crop_size, self.crop_size)) if crop_size else transforms.Resize((224, 224)),
-                    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.05)
-                ])
+def apply_transforms(self, img, gt):
+                augmented = Image.fromarray(np.concatenate((np.array(img), np.array(gt)[:, :, np.newaxis]), axis=2))
+                if self.data_transforms is not None:
+                        augmented = self.data_transforms(augmented)
+                
+                augmented = np.array(augmented)
+                img = Image.fromarray(augmented[:, :, :3])
+                gt = Image.fromarray(augmented[:, :, 3])
+                
+                return img, gt
