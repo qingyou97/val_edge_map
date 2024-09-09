6d8bb85fc619baf59668566ecd5a734f8fb6820b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-09 17:20:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import matplotlib.pyplot as plt
-import torch
+import numpy as np
 
-# 将图像加载换为CPU且变成numpy数组
-image0_cpu = image0.squeeze().cpu().numpy().transpose(1, 2, 0)
-image1_cpu = image1.squeeze().cpu().numpy().transpose(1, 2, 0)
+def plot_matches(image0, image1, points0, points1):
+    # 将图像转换为numpy格式
+    image0_np = image0.cpu().numpy().transpose(1, 2, 0)  # convert (3,H,W) -> (H,W,3)
+    image1_np = image1.cpu().numpy().transpose(1, 2, 0)
 
-# 画图
-fig, ax = plt.subplots(1, 2, figsize=(15, 10))
+    # 创建合并图像，左右并行显示 input images
+    height = max(image0_np.shape[0], image1_np.shape[0])
+    width = image0_np.shape[1] + image1_np.shape[1]
+    composite_image = np.zeros((height, width, 3))
 
-# 绘制图像1的点
-ax[0].imshow(image0_cpu)
-ax[0].scatter(points0[:, 0], points0[:, 1], c='r', marker='o')
-ax[0].title.set_text('Image 0 Keypoints')
+    composite_image[:image0_np.shape[0], :image0_np.shape[1], :] = image0_np
+    composite_image[:image1_np.shape[0], image0_np.shape[1]:, :] = image1_np
 
-# 绘制图像2的点
-ax[1].imshow(image1_cpu)
-ax[1].scatter(points1[:, 0], points1[:, 1], c='r', marker='o')
-ax[1].title.set_text('Image 1 Keypoints')
+    # 调整点坐标, 在框取合成图上，特征点1需偏移以匹配image_1的位置
+    points1_adj = points1.clone().numpy()
+    points1_adj[:, 0] += image0_np.shape[1]
 
-# 在两图之间画连线
-for p0, p1 in zip(points0, points1):
-    fig.lines.append(plt.Line2D(
-        [p0[0], p1[0] + image0_cpu.shape[1]], 
-        [p0[1], p1[1]], 
-        color='yellow'  # 线条颜色可以自己更改
-    ))
+    # 作图
+    fig, ax = plt.subplots(figsize=(18,9))
+    ax.imshow(composite_image)
 
-# 拼接两幅图像
-combined_image = torch.cat((image0, image1), dim=2).squeeze().cpu().numpy().transpose(1, 2, 0)
-ax_combined = fig.add_subplot(111, frameon=False)
-ax_combined.imshow(combined_image)
-ax_combined.axis('off')
-ax_combined.title.set_text('Correspondences')
+    # 绘制点及匹配线条
+    for (pt0, pt1) in zip(points0.cpu().numpy(), points1_adj):
+        color = np.random.rand(3)
+        ax.scatter(*pt0, s=10, facecolors=color, edgecolors='k')
+        ax.scatter(*pt1, s=10, facecolors=color, edgecolors='k')
+        ax.plot([pt0[0], pt1[0]], [pt0[1], pt1[1]], color=color, linewidth=1.5)
 
-plt.show()
+    ax.axis('off')
+    plt.show()
+
+# 调用到函数的是,例如以下方法
+plot_matches(image0, image1, points0, points1)
