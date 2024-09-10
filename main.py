148600ebe69584c0dba6ac137640eba83046f95c
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-10 18:55:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# 将 a 作为输入传给模型
-    output = model.forward(a)
+x2 = torch.nn.functional.interpolate(x2, size=(128, 128), mode='bilinear', align_corners=False)
 
-    # 如果你想打打印出所有输出，可以使用以下循环进行打印：
-    for i, out in enumerate(output):
-        print(f'Output {i}: {out.size()}')
-        print(out)
+        x = torch.cat((x1, x2), dim=1)
