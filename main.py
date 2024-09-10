7d2a6603c0057622bf74c04c71b4855a58a3b246
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-10 20:05:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-x2 = torch.nn.functional.interpolate(x2, size=(128, 128), mode='bilinear', align_corners=False)
-
-        x = torch.cat((x1, x2), dim=1)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
