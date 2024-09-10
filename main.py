e1fcabd4c67d61c7fc0e519fd1fc2dceea378290
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-10 15:19:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-https://github.com/ashishpatel26/Tools-to-Design-or-Visualize-Architecture-of-Neural-Network
+# 将 a 作为输入传给模型
+    output = model.forward(a)
+
+    # 如果你想打打印出所有输出，可以使用以下循环进行打印：
+    for i, out in enumerate(output):
+        print(f'Output {i}: {out.size()}')
+        print(out)
