ffd24da505ff4a139de74ae8af221038ec3d4ccc
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-11 13:54:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-I have a question. You mentioned this morning that in a classifier we should write a simple decoder to help combine features, using upsampling within it. We need to use the nearest neighbor interpolation method. I thought about it, since sum1 to sum5 are all 21 channels but different in size. If we use a fixed conv in the encoder and interpolate and concat in the decoder, it becomes multi-channel again. Unless we consider adding each feature map, then it can stay single-channel. Is this reasonable? Or should the classifier do the sampling first, then go through a conv to output a single channel?
+class FeatureExtractor(nn.Module):
+    def __init__(self, num_classes):
+        super(FeatureExtractor, self).__init__()
+
+        self.conv = nn.Conv2d(128, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, middle_output_list):
+        upsampled_map_list = []
+        for i, map in enumerate(middle_output_list):
+            upsampled_map = F.interpolate(map, size=(512, 512), mode='nearest', align_corners=False)
+            upsampled_map_list.append(upsampled_map)
+        upsampled_map_cat = torch.cat(upsampled_map_list, dim=1)
+        map = self.conv(upsampled_map_cat)
+
+        return map
