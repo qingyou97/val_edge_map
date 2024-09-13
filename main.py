19371937f4031fb3a7f1fc5d24c6261e851035aa
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-09-13 17:23:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import torch
-from torchvision import models
-from torchvision.models.feature_extraction import create_feature_extractor
+1. Combined wideresnet and bdcn, implemented inference code, considered fusion strategy, then passed through classifier to check results.
+a. Today, I finished the inference code using three layers of features from Wideresnet as classifier inputs. I initially encountered out-of-memory errors, which were due to using hooks to extract Wideresnet features, causing memory to not be released. I switched to using the create_feature_extractor method from torchvision. Training and inference code are now complete.
 
-# 加载预训练的Wide ResNet模型
-model = models.wide_resnet101_2(pretrained=True)
-print(model)
+b. I tested Wideresnet directly as a classifier. Results are in Excel sheet1, rows 4-5-6. It fitted well to training images but had poor generalization to other images. The edges of other images were discontinuous.
 
-# 指定你感兴趣的层
-return_nodes = {'layer1': 'layer1_output'}
-
-# 创建特征提取器
-feature_extractor = create_feature_extractor(model, return_nodes)
-
-# 输入数据，进行前向传播
-input_data = torch.rand(1, 3, 512, 512)
-features = feature_extractor(input_data)
-
-# 获取特定层的输出
-layer_output = features['layer1_output']
-print(layer_output.shape)
+c. I then tried merging Wideresnet and BDCN using sum5 and trained the classifier. Results are in sheet1, rows 7-8-9-10-11. For single image training, the best result is in row 9 because I used a high contrast image, enhancing the model's generalization ability. I then trained with another image representing a different type in the dataset, and added dropout. The best result is in row 10, detecting the desired double circles, but with some edges not fully suppressed.
