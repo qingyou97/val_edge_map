10c3f4c1c8e75f96de6333107ac5d84e43562b2c
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-08 16:23:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-import os
-from PIL import Image
+import cv2
 import numpy as np
 
-# 文件夹路径
-gt_folder = 'C'
-result_folder = 'D'
-output_folder = 'E'
+def color_transform(image_path):
+    # 读取图像
+    image = cv2.imread(image_path)
 
-# 创建输出文件夹，如果不存在的话
-os.makedirs(output_folder, exist_ok=True)
+    # 定义深紫色和黄色的颜色范围
+    deep_purple_lower = np.array([120, 0, 120])
+    deep_purple_upper = np.array([180, 100, 180])
 
-# 获取文件列表
-gt_files = {f.split('_')[0]: f for f in os.listdir(gt_folder) if f.endswith('_gt.png')}
-result_files = {f.split('_')[0]: f for f in os.listdir(result_folder) if f.endswith('_result_sobel_result.png')}
+    yellow_lower = np.array([0, 220, 220])
+    yellow_upper = np.array([200, 255, 255])
 
-# 处理每一对文件
-for key in gt_files.keys():
-    if key in result_files:
-        gt_path = os.path.join(gt_folder, gt_files[key])
-        result_path = os.path.join(result_folder, result_files[key])
-        
-        # 加载图像
-        gt_image = Image.open(gt_path).convert('RGB')
-        result_image = Image.open(result_path).convert('RGB')
-        
-        # 转换成numpy数组
-        gt_np = np.array(gt_image)
-        result_np = np.array(result_image)
-        
-        # 创建红色注释图层
-        red = np.array([255, 0, 0])
-        
-        # 找到gt_image中不是黑色的像素点
-        mask = (gt_np != [0, 0, 0]).any(axis=-1)
-        
-        # 在result_image中用红色标注这些点
-        result_np[mask] = red
-        
-        # 保存新图像
-        output_image = Image.fromarray(result_np)
-        output_path = os.path.join(output_folder, f'{key}_highlighted.png')
-        output_image.save(output_path)
+    # 创建掩码并应用颜色转换
+    mask_purple = cv2.inRange(image, deep_purple_lower, deep_purple_upper)
+    image[mask_purple > 0] = [0, 0, 0]
 
-        print(f"Processed and saved: {output_path}")
+    mask_yellow = cv2.inRange(image, yellow_lower, yellow_upper)
+    image[mask_yellow > 0] = [255, 255, 255]
+    
+    return image
+
+# 使用示例
+img_path = 'path_to_your_image.jpg'
+transformed_image = color_transform(img_path)
+cv2.imwrite('transformed_image.jpg', transformed_image)
