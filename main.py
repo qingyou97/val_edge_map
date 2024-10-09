aa572c409b3b7f8398e211d6e1e28087541d2da4
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-09 11:56:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import os
-from PIL import Image, ImageChops
-
-def filter_images_by_ai_result(original_folder, ai_result_folder, output_folder, label_color=(253, 231, 36, 255)):
+def overlay_original_on_ai_result(original_folder, ai_result_folder, output_folder):
     """
-    过滤图像，使得 original_folder 中的图像只保留 ai_result_folder 中指定颜色的像素位置，其他位置置为黑色。
+    将 original_folder 中的图像中的有值像素叠加到 ai_result_folder 中的图像上，以查看重合度。
 
     :param original_folder: 包含原始图像的文件夹路径
     :param ai_result_folder: 包含 AI 结果图像的文件夹路径
     :param output_folder: 输出结果图像的文件夹路径
-    :param label_color: 需要匹配的颜色，默认为 (253, 231, 36, 255)
     """
     # 确保输出文件夹存在
     os.makedirs(output_folder, exist_ok=True)
@@ -24,27 +20,24 @@ def filter_images_by_ai_result(original_folder, ai_result_folder, output_folder,
         original_image = Image.open(original_path).convert('RGBA')
         ai_result_image = Image.open(ai_result_path).convert('RGBA')
         
-        # 新图像，用来存储修改后的结果
-        result_image = Image.new('RGBA', original_image.size)
+        # 创建一个新的图像，用于叠加结果
+        overlay_image = ai_result_image.copy()
         
         original_pixels = original_image.load()
-        ai_result_pixels = ai_result_image.load()
-        result_pixels = result_image.load()
+        overlay_pixels = overlay_image.load()
         
         for y in range(original_image.height):
             for x in range(original_image.width):
-                if ai_result_pixels[x, y] == label_color:
-                    result_pixels[x, y] = original_pixels[x, y]
-                else:
-                    result_pixels[x, y] = (0, 0, 0, 255)
+                if original_pixels[x, y][3] != 0:  # 检查 alpha 通道是否不为 0
+                    overlay_pixels[x, y] = original_pixels[x, y]
         
         # 保存结果文件
-        result_image.save(os.path.join(output_folder, original_file.replace('_original', '_filtered')))
+        overlay_image.save(os.path.join(output_folder, original_file.replace('_original', '_overlay')))
 
     print("操作完成，每对图像已处理并保存。")
 
 # 使用示例
 original_folder = 'A文件夹'
 ai_result_folder = 'B文件夹'
-output_folder = '结果文件夹'
-filter_images_by_ai_result(original_folder, ai_result_folder, output_folder)
+output_folder = '叠加结果文件夹'
+overlay_original_on_ai_result(original_folder, ai_result_folder, output_folder)
