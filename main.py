88959d5b35a53b2a8d79a9b59e8e1b06a3ec0a37
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-09 10:37:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-1. Apply rule-based methods directly on AI results, and apply rule-based methods directly on the original image. Compare with GT to determine if the thin edge locations are desired.
-2. When doing rule-based, find peak intensity and explore direction. Use Sobel on the original image to get intensity and direction, and check if you can decide peak and direction to adjust the edge.
-3. Look for other methods to find matching thin edges.
+1009 Morning Meeting 10:00-10:30:
+Duan:
+1. In the AI result area, combine the Sobel result on the edge of the original image, find the point with the maximum peak intensity on the gradient direction line, keep this point, review the result, and compare it with the GT.
+
+Han Xu:
+2. Discuss the goals with Duan: oneshot reinforcement, rule-based combination, and discuss milestones tomorrow.
+3. For DCAMA, change the backbone to PIDNet.
+4. Modify Instance Normalization again, the position is incorrect.
