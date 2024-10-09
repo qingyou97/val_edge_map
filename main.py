19c27ef7bfe34ff44448cc213625d08d7c3ee3f6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-09 15:41:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-# 在左上角标注坐标
-    text = f'({x},{y})'
-    text_position = (5, 15 + coords.index(item) * 20)  # 计算每个坐标的文字位置，避免重叠
-    cv2.putText(img_anno, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+magnitude_save = np.uint8(np.clip(magnitude / magnitude.max() * 255, 0, 255))
+    cv2.imwrite('edge_magnitude.png',magnitude_save)
