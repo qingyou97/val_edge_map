30a84113db79c753846a1ca4e3a63bbdecd612ac
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-09 09:44:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-Using a rule-based method on AI results, determine if it can refine to a specified GT edge position. 
+1. Apply rule-based methods directly on AI results, and apply rule-based methods directly on the original image. Compare with GT to determine if the thin edge locations are desired.
+2. When doing rule-based, find peak intensity and explore direction. Use Sobel on the original image to get intensity and direction, and check if you can decide peak and direction to adjust the edge.
+3. Look for other methods to find matching thin edges.
