cffae7113d49174132e3bebb4801c3f937d68950
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-10 18:33:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# 调整窗口大小
-cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Image', 800, 600)  # 例如，设置窗口大小为800x600
-
-# 显示图像
-cv2.imshow('Image', image)
-
-# 等待键盘输入以便关闭显示窗口
-cv2.waitKey(0)
-
-# 销毁所有窗口
-cv2.destroyAllWindows()
+# 获取最大三个值的索引
+    top_3_indices = np.argsort(intensities)[-3:]
+    
+    # 倒序排列以确保第一个索引是最大值，其次是其次大值
+    top_3_indices = top_3_indices[::-1]
+    
+    # 根据索引获取相应的点
+    top_3_points = [coords[i] for i in top_3_indices] 
