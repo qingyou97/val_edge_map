b8ccd473f3c6fb367048e46024b9b54ea3275a11
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-11 09:16:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-1. Overlay GT and original image to confirm which layer GT is on:
-Conclusion: First, the GT I drew is not precise and overlaps the artifact outlines. Second, dilation causes learning of edges on both layers. (Images in the xxx file’s sheet at row 29). Third, GT adjustments will be completed this morning.
-
-2. Handwritten rule: Keep points closer to the inner diameter.
-Conclusion: Done. Comparatively, it's better than taking the maximum peak point. (Images in the xxx file’s sheet at row 29)
-Circle center rule: Read grayscale image and apply morphological opening and closing operations to denoise. Use thresholding to convert the image to binary and apply Laplace transform for edge detection to find contours. Fit each contour to an ellipse and select the one with the smallest axis ratio as the most circular ellipse.
-Point filtering rule: Take points in both positive and negative gradient directions until five continuous points with an intensity of 0 are found. From these points, find two peak points and select the one closer to the circle center. (Explanation is in the last section of the xxx word file)
-Effect: Better than taking the maximum peak point but retained some outer points. Since some gradients were selected in non-perpendicular directions to the contour, our logic couldn't interfere, causing extra points. (Explanation is in the last section of the xxx word file)
-
-3. Team management meeting and data organization.
-
-4. Handwritten rule: Retain points from light to dark on the innerside edges; if AI doesn’t work, use rule-based customization to solve edges.
-Conclusion: Not started yet.
-
-5. Experiment with Han’s Dexined results.
-Conclusion: Not received yet.
+Third, we will complete the fine-tuning of gt this morning. There is a problem. If I label the single-pixel contours on the original 512*512 image, Hanxu will need to resize it to 224*224 later. This causes the same issue with breakpoints. To avoid this, we need to do bicubic interpolation and then convert it to a binary image, but this makes the edges not single-pixel. Can I directly label the single-pixel edges on the 224*224 original image instead, which would be more suitable?
