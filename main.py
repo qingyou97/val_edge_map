9ea75069f1b9e24b8d0c8494e13b4606d8ed99ba
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-11 10:56:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-1. Re-label the casting data.
-2. Currently retain the scheme of keeping the maxima closer to the inner diameter, needs further fine-tuning.
-3. Custom scheme of alternating light and dark zones.
+Morning meeting 10-11, 10.00-10.40:
+
+Han:
+1. The idea of dcama lightweighting doesn't need to be so constrained and concluded quickly. Take more time to think it through. Try adding something similar to conv.
+2. In the summary document, clarify which architectures or parameters were changed and what trends resulted from these changes. Not just the phenomena. After summarizing the trends, you can propose solutions.
+
+Duan:
+1. Currently, maintain the scheme that keeps the maximum point nearest to the inner diameter but needs further fine-tuning.
+2. Customized scheme with alternating light and dark areas.
+3. Try other datasets like groove and cylinder since it's proven to be effective on casting. Check if it works on other datasets. Fine-tuning can be done later.
