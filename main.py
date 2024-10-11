ba6f7bd8c2ed3346f89d9d7762a4a534fda1c22c
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-11 11:36:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-Morning meeting 10-11, 10.00-10.40:
-
-Han:
-1. The idea of dcama lightweighting doesn't need to be so constrained and concluded quickly. Take more time to think it through. Try adding something similar to conv.
-2. In the summary document, clarify which architectures or parameters were changed and what trends resulted from these changes. Not just the phenomena. After summarizing the trends, you can propose solutions.
-
-Duan:
-1. Currently, maintain the scheme that keeps the maximum point nearest to the inner diameter but needs further fine-tuning.
-2. Customized scheme with alternating light and dark areas.
-3. Try other datasets like groove and cylinder since it's proven to be effective on casting. Check if it works on other datasets. Fine-tuning can be done later.
+# Compute angle from A (y, x) to B (y2, x2)
+    delta_y = y2 - y
+    delta_x = x2 - x
+    angle_rad = math.atan2(delta_y, delta_x)
+    angle_deg = np.degrees(angle_rad)
+    
+    # The angle_deg should be in range [0, 180)
+    angle_deg = angle_deg % 180
