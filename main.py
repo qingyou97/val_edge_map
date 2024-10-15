234d81cab00e08e90555cfae11b28c5584d8ac24
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-15 16:33:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import os
+def extract_points_magnitude_and_angle(magnitude, direction, points, shape):
+    new_magnitude = np.zeros(shape)
+    new_angle = np.zeros(shape)
 
-folder_path = 'your_folder_path'  # 替换为你的文件夹路径
+    for point in points:
+        new_magnitude[point[1], point[0]] = magnitude[point[1], point[0]]
+        new_angle[point[1], point[0]] = direction[point[1], point[0]]
+    
+    return new_magnitude, new_angle
 
-for filename in os.listdir(folder_path):
-    if filename.lower().endswith('.jpeg'):
-        new_filename = filename.rsplit('.', 1)[0] + '.jpg'
-        os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
-        print(f'Renamed: {filename} -> {new_filename}')
+# 使用例子
+img_path = 'your_image.jpg'
+points = [(x1, y1), (x2, y2), ..., (xn, yn)]  # 替换为实际坐标点列表
+
+magnitude, angle, color_image, image = caculate_magnitude_and_direction(img_path)
+new_magnitude, new_angle = extract_points_magnitude_and_angle(magnitude, angle, points, magnitude.shape)
+
+# 保存结果图像或进行其他处理
+cv2.imwrite('new_magnitude_image.jpg', new_magnitude)
+cv2.imwrite('new_angle_image.jpg', new_angle)
