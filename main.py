221ab3cee5e53607f02d8e3cf13b081ed66bdaa7
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-15 10:12:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-1. Casting dataset - Currently, we keep the local maxima points closer to the inner diameter, and further fine-tuning is needed.
-Conclusion: Changing the gradient towards the center improved the results, filtering out more outer edges.
-
-2. Casting dataset - Alternating bright and dark algorithm to determine edge retention.
-Conclusion: The first custom version retains only left-bright-right-dark on the left half of the image, and left-dark-right-bright on the right half. Also need logic for up and down. Results are mediocre, with intermittent gaps.
-The second version determines which edge is closer to the center with darker pixels and further with brighter pixels, removing the need for directional logic. Currently testing this.
-
-3. Try other datasets like groove and cylinder, as the method currently shows good results on casting. Fine-tuning can be done later.
-Conclusion: Using dexined results, tested on groove and cylinder.
-Cylinder: The first version took the highest peak, showing a double edge. The second version detects an ellipse and keeps the edge closer to the center.
-Groove: Currently, it takes the highest peak points.
+4. Relabeled 20 images of casting. All are 224*224 in size with a black background and white single-pixel edges. Ensured all are inner edges.
