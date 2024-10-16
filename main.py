0dc97ea4560065879f2d3d2143069d9d3dbf6380
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-16 10:47:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-The best logic for the three datasets was run on Han Xu's latest version of dexined last night. Not yet organized.
+10/16 - Morning Meeting 10:00-10:40:
+Duan:
+1. Overlay the known results with the original gt image to see if the desired gt has been extracted.
+2. For the cylinder dataset, check the gt annotations and see if lighting issues affect gt extraction.
+3. Organize yesterday's new Dexined results from Han Xu and make comparisons.
+4. Determine if some noise issues can be solved by a rule-based method.
+
+Han Xu:
+5. Test with multiple support, and then provide it to Duan. Duan will do the rule-based part.
+6. Improve the tuning for the merge layer algorithm, e.g., the edges detected by Dexined should not be filtered out during merging.
