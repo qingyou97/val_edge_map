8198bb334419d3e9d100707ece8e70d3813a0ad8
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-17 18:47:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import os
-import shutil
+def eliminate(image_path, save_path):
+    # 读取灰度图
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# 定义原本的和目标目录
-source_dir = os.path.join('A')
-folders = ['0_bottle', '1_casting', '2_cylinder', '3_groove', '4_aero', '5_ball_screw']
+    # 二值化
+    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
-# 在A目录下创建新的文件夹
-for folder in folders:
-    folder_path = os.path.join(source_dir, folder)
-    os.makedirs(folder_path, exist_ok=True)
+    ori = img.copy()
 
-    # 需要复制的文件夹
-    subfolders = ['ai_result', 'gt', 'original']
-    
-    # 复制每个文件夹到新的文件夹里面
-    for subfolder in subfolders:
-        src = os.path.join(source_dir, subfolder)
-        dst = os.path.join(folder_path, subfolder)
-        if os.path.exists(src):
-            shutil.copytree(src, dst, dirs_exist_ok=True)
+    # 先腐蚀操作后膨胀操作
+    kernel = np.ones((2, 2), np.uint8)
+    img = cv2.erode(img, kernel, iterations=1)  # 程度重
+    img = cv2.dilate(img, kernel, iterations=1)
+
+    # 滤波
+    img = cv2.medianBlur(img, 3)  # 程度轻
+
+    # 保存结果
+    cv2.imwrite(save_path, img)
+
+    # 显示结果
+    cv2.imshow('Original Image', ori)
+    cv2.imshow('After Erosion and Dilation', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+image_path = r'D:\Projects\edge_detection\rule_based\images\20241017-173513.jpg'
+save_path = r'a.png'
+eliminate(image_path, save_path)
