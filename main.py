1c2dfa505e62189288fff816d1869596d1466a01
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-17 11:09:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Every single detail is reflected in each chart in Excel.
+Morning meeting 10-30 to 11-05 on 10-17:
+Duan:
+1. If Han Xu's multi-support results are available, continue testing and comparison analysis (test both pidinet and dexined). Find problems and discuss within the team whether to improve Han’s AI or Duan’s rule.
+2. Test other datasets with ball-screw.
+3. Perform some morphological operations on AI results to remove redundant points.
+Han Xu:
+4. Provide Duan with the best version of multi-support; Duan to continue with rules.
+5. Improve the merging part to ensure zero-shot can be detected and output in the final result.
