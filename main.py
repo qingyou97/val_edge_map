cceee3bc887121e33f60764d768d3da2878904e3
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-17 16:52:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-Morning meeting 10-30 to 11-05 on 10-17:
-Duan:
-1. If Han Xu's multi-support results are available, continue testing and comparison analysis (test both pidinet and dexined). Find problems and discuss within the team whether to improve Han’s AI or Duan’s rule.
-2. Test other datasets with ball-screw.
-3. Perform some morphological operations on AI results to remove redundant points.
-Han Xu:
-4. Provide Duan with the best version of multi-support; Duan to continue with rules.
-5. Improve the merging part to ensure zero-shot can be detected and output in the final result.
+import os
+import shutil
+
+# 定义原本的和目标目录
+source_dir = os.path.join('A')
+folders = ['0_bottle', '1_casting', '2_cylinder', '3_groove', '4_aero', '5_ball_screw']
+
+# 在A目录下创建新的文件夹
+for folder in folders:
+    folder_path = os.path.join(source_dir, folder)
+    os.makedirs(folder_path, exist_ok=True)
+
+    # 需要复制的文件夹
+    subfolders = ['ai_result', 'gt', 'original']
+    
+    # 复制每个文件夹到新的文件夹里面
+    for subfolder in subfolders:
+        src = os.path.join(source_dir, subfolder)
+        dst = os.path.join(folder_path, subfolder)
+        if os.path.exists(src):
+            shutil.copytree(src, dst, dirs_exist_ok=True)
