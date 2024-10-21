706ae4e897a1d028808821772ffcc97435949f5a
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-21 17:43:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-rect_width = 10  # 这里设定你的最大值
-length = 5  # 要取出的序列长度
+inner_radius = 1  # 设定inner_radius
+outer_radius = 5  # 设定outer_radius
+rect_width = 10   # 设定rect_width
+length = 5        # 要取出的序列长度
 
-for i in range(rect_width + 1):
-    sequence = [(j + i) % (rect_width + 1) for j in range(length)]
-    print(sequence)
+for y in range(inner_radius, outer_radius + 1):
+    for i in range(rect_width + 1):
+        sequence = [(j + i) % (rect_width + 1) for j in range(length)]
+        for x in sequence:
+            print((x, y))
