a09b32ae750ed8f305083c4e1ebddc9f45cf04a1
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-21 17:44:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-inner_radius = 1  # 设定inner_radius
-outer_radius = 5  # 设定outer_radius
-rect_width = 10   # 设定rect_width
-length = 5        # 要取出的序列长度
-
-for y in range(inner_radius, outer_radius + 1):
-    for i in range(rect_width + 1):
-        sequence = [(j + i) % (rect_width + 1) for j in range(length)]
-        for x in sequence:
-            print((x, y))
+for x in range(rect_width + 1):
+    sequence = [(j + x) % (rect_width + 1) for j in range(length)]
+    for y in range(inner_radius, outer_radius + 1):
+        for value in sequence:
+            print((value, y))
