2f30c959846cd90ece793b1dc7e2fb11592fc223
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-21 17:46:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for x in range(rect_width + 1):
-    sequence = [(j + x) % (rect_width + 1) for j in range(length)]
-    for y in range(inner_radius, outer_radius + 1):
-        for value in sequence:
-            print((value, y))
+for i in range(rect_width + 1):
+    sequence = [(j + i) % (rect_width + 1) for j in range(length)]
+    for x in sequence:
+        for y in range(inner_radius, outer_radius + 1):
+            print((x, y))
