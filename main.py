c2f5738bd8af4b2a920d34d096c8595a16284f2c
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-21 16:08:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ if len(ellipses) >= 4:
     # 按面积排序，确保第一个和第二个是一个圆环的内外轮廓，第三个和第四个是另一个圆环的内外轮廓
     ellipses = sorted(ellipses, key=lambda e: e[1][0] * e[1][1], reverse=True)
     
+    def make_circle(ellipse):
+        center, axes, angle = ellipse
+        max_radius = max(axes)
+        return (center, (max_radius, max_radius), angle)
+    
+    # 调整为正圆
+    ellipses[0] = make_circle(ellipses[0])
+    ellipses[1] = make_circle(ellipses[1])
+    ellipses[2] = make_circle(ellipses[2])
+    ellipses[3] = make_circle(ellipses[3])
+    
     # 绘制第一个圆环到第一个图像
     cv2.ellipse(output_image1, ellipses[0], 255, 2)  # 用白色绘制外轮廓
     cv2.ellipse(output_image1, ellipses[1], 255, 2)  # 用白色绘制内轮廓
