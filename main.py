5190ae6bda35a88ba2af59c30b08d2f48745d06e
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-22 14:51:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-10.22 Morning Meeting 10:00-10:40:
-Duan:
-1. Test and organize the results of the polar coordinate algorithm
-2. List the follow-up experiment plan for rule-based, and send files to Mr. Zhao
-3. Organize a list of deliverables
-Han Xu:
-4. Improve the merge part to ensure that what zeroshot can detect can also be output
-5. Organize the follow-up list of dcama experiments
-6. Organize a list of deliverables
+A summary table of problems and countermeasures
