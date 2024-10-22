d065e881827a199428b7ebb33d24086f145b37d5
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-22 11:00:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-1. Confirm the issues with casting and some breakpoints to determine if they are caused by AI or rule-based systems.
-Conclusion: Two reasons. First, AI results are redundant, leading to many points being considered during the extension line calculation, including strong AI-passed areas closer to the center, which are retained. Second, rule-based detection picks the top two edge intensity points on the extension line as outside edges, while the inner edge is ranked third, thus picking the outer edge. File link:
-2. Tried setting peak points to the top three for issue 1.
-Conclusion: Improved individual images like 15 and 17, where previously the top 2 did not include the inner edge, causing edge deviation. Now selecting the nearest top 3 to the center improves such images. But AI result redundancy causing edge noise remains unsolved.
-3. Polar coordinate algorithm has issues. Should use interpolation to expand the half-sector into a rectangle, enveloping the edge area. Missed points in the half-sector should be interpolated for results. Then average and select the maximum as the edge.
-Conclusion: Testing in progress; found and fixing bugs.
-4. v6 data: Apply a threshold on oneshot AI results to filter some noise and analyze results.
-Conclusion: Chose a threshold of 75 to keep fewer disconnects while maintaining thicker edges. The current result is not better than the previous best 4 support merge version but is better than other versions, though noisier compared to the optimal 4 support merge.
+10.22 Morning meeting 10.00-10.40:
+Duan:
+1. Test and organize the results of the polar coordinates algorithm
+2. List out the experimental plans for rule-based, organize the files and send to Mr. Zhao
+Han Xu:
+3. Improve the merge part, ensure zero-shot can be detected and output
+4. Organize the follow-up list for DCAMA experiments
