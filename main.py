9c69c20c3517bd5ecbe0ef905b29194d0e5b3a40
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-23 11:39:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-10/23 Morning Meeting 10:00-10:40:
-
-Duan:
-1. When selecting peaks, set a threshold and pick the first peak that exceeds this threshold.
-2. The length of the sliding window should be adjustable and the effects of each change need to be recorded.
-3. In the previous Gaussian denoising step, parameter tuning can improve Sobel results.
-
-Han:
-1. Summarize deliverables based on viewpoints.
-2. Organize before-and-after comparison charts.
-3. Summarize examples of both significant improvements and weakened results; choose two charts showing significant improvement and two showing significant weakening.
-4. Focus on improvements in loss and kernel size; organize comparisons of these two aspects before and after.
+1. 选取峰值时，应当设置一个阈值，超过这个阈值后，筛选第一个峰值
+2. 滑窗的长度需要设置为可调参数，并且记录下来每次这个参数变化的效果
