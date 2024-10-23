6bcadbf82e70829c3deefc44687f073fc91bef92
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-23 14:21:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
-def find_y_value(d, thresh):
-    # 把强度值除以5
-    d = {k: v / 5 for k, v in d.items()}
-    
-    # 按y的值从小到大排序
-    sorted_items = sorted(d.items())
-    
-    # 遍历所有y的值，找到第一个符合条件的
-    for y, intensity in sorted_items:
-        if intensity > thresh:
-            return y
-    
-    # 如果没有找到符合条件的值，返回None
-    return None
+inner_radius = 1  # 设定inner_radius
+outer_radius = 5  # 设定outer_radius
+rect_width = 10   # 设定rect_width
+length = 5        # 要取出的序列长度
 
-
-# 示例字典
-d = {
-    58: 326.8625193847768,
-    59: 88.41092178101273,
-    60: 511.08902684585762,
-    61: 624.0675913836851,
-    62: 1831.24699056712966,
-    63: 1961.3456440528098,
-    64: 817.7538061704564,
-    65: 213.56610930077673,
-    66: 582.4233078671347,
-    67: 9422.5185305268745,
-    68: 1038.215331300595
-}
-
-# 设定阈值
-thresh = 200.0
-
-# 调用函数
-result = find_y_value(d, thresh)
-print(result)
+for i in range(rect_width + 1):
+    sequence = [(j + i) % (rect_width + 1) for j in range(length)]
+    for x in sequence:
+        for y in range(inner_radius, outer_radius + 1):
+            print((x, y))
