885e0f227acfd87fa9ea755b45c1112cccca6cc6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-23 13:36:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from PIL import Image
+def find_min_intensity(data):
+    """
+    从包含坐标和强度值的列表中找到强度值的最小值及其对应的坐标。
+    
+    :param data: list of lists, 形如 [[(x, y), intensity], ...]
+    :return: (tuple, float), 最小强度值和对应的坐标
+    """
+    if not data:
+        return None, None
 
-# 打开 A 图和 B 图
-a_image = Image.open('A.png')
-b_image = Image.open('B.png')
+    min_intensity = float('inf')
+    min_coordinate = None
 
-# 加载像素
-a_pixels = a_image.load()
-b_pixels = b_image.load()
+    for item in data:
+        coordinate, intensity = item
+        if intensity < min_intensity:
+            min_intensity = intensity
+            min_coordinate = coordinate
 
-# 遍历检查每个像素
-width, height = a_image.size
-red_points = []
-for x in range(width):
-    for y in range(height):
-        if a_pixels[x, y] == (255, 0, 0):  # 查找正红色点
-            red_points.append((x, y))
-            b_pixels[x, y] = (255, 0, 0)  # 在 B 图上涂上红色
+    return min_coordinate, min_intensity
 
-# 保存 B 图
-b_image.save('B_with_red.png')
+# 示例用法
+data = [[(220, 116), 284.0633732109791], 
+        [(215, 122), 180.0567890], 
+        [(210, 118), 300.56473820]]
+
+coordinate, min_intensity = find_min_intensity(data)
+print(f"最小强度值为: {min_intensity}, 对应的坐标为: {coordinate}")
