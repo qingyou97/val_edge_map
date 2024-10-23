4aeaf3987440bf08e9063c6fea65b970fce41b4a
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-23 09:33:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-Hello, here is our follow-up experiment plan for today. It includes the rule-based preliminary schemes and the scheduling. It also covers the plan for integrating 1-shot and 0-shot methods, and the experiment plan for dcama. Both of our plans are included in this Excel sheet.
+1. Testing and organizing results of the polar coordinate algorithm
+Conclusion: The results for a single image processed on my computer are done (processing for other images continues on someone else's machine). The effect is not better than the previous version based on finding peak points via the circle center. However, it clearly avoids a specific scenario, "if many consecutive pixels are good, but only one or two pixels are offset," this situation can be avoided. But when a large area of the region has larger peak points on the outer edge, it completely biases towards the outer contour. File link:
+
+2. List the follow-up experimental plans for rule-based, and send the organized files to Mr. Zhao
+Conclusion: Yesterday, I thought about and did some small experiments, resulting in three subsequent schemes. They are organized in this file. File link:
