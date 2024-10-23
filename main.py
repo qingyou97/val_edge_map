43ec9e08b8be7412f76a32e61b65411f8c255ed3
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-23 15:47:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-inner_radius = 1  # 设定inner_radius
-outer_radius = 5  # 设定outer_radius
-rect_width = 10   # 设定rect_width
-length = 5        # 要取出的序列长度
+from PIL import Image
 
-for i in range(rect_width + 1):
-    sequence = [(j + i) % (rect_width + 1) for j in range(length)]
-    for x in sequence:
-        for y in range(inner_radius, outer_radius + 1):
-            print((x, y))
+# 打开黑白图像
+img = Image.open('A图.png').convert('RGBA')
+
+# 定义颜色替换规则
+color_mapping = {
+    (255, 255, 255, 255): (253, 231, 36, 255),
+    (0, 0, 0, 255): (68, 1, 84, 255)
+}
+
+# 获取图像数据
+data = img.getdata()
+
+# 创建一个新的图像数据列表
+new_data = []
+for item in data:
+    if item in color_mapping:
+        new_data.append(color_mapping[item])
+    else:
+        new_data.append(item)
+
+# 创建新的图像对象并赋予新的数据
+new_img = Image.new("RGBA", img.size)
+new_img.putdata(new_data)
+
+# 保存为新的PNG图像
+new_img.save('A图_new.png')
