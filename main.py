a13a51882f21db65e6aacf2c98829267dcb6c3b6
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-25 17:07:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-def sum_strengths_by_x(coordinates_dict):
-    sum_by_x = {}
-    
-    # 遍历字典中的每个元素
-    for (x, y), strength in coordinates_dict.items():
-        # 如果 x 没有在结果字典中，初始化为0
-        if x not in sum_by_x:
-            sum_by_x[x] = 0.0
-        
-        # 将当前强度值加到对应 x 的总和中
-        sum_by_x[x] += strength
-    
-    return sum_by_x
+# 在图像上绘制椭圆
+for ellipse in ellipses:
+    cv2.ellipse(image, ellipse, (255, 255, 255), 2)
