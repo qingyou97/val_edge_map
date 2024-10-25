c67e6bac5e4b63caa2fa1957aa8ecd1255e2bde4
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-25 13:40:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-1025 Morning Meeting from 10:00 to 10:40:
+start_x = rect[0][0]
+end_x = rect[1][0]
 
-Duan:
-1. Further optimize the algorithm for finding the four contours of the ring.
-2. Fix ROI in the groove dataset first, then test top and bottom edges to check detection effect.
-
-Han:
-3. Compare dCAMa results, organize previous and today's results for comparison.
-4. Add instnorm after each conv, test the effect.
-5. Usually, clip + tanh causes instability; suggest switching to sigmoid.
-6. Add continuity constraints to the edge, converting them to loss.
+for i in range(start_x, end_x + 1):
+    line = list(range(i, i + 5))
+    if line[-1] > end_x + 1:
+        break
+    print(line)
