9c9640c77f8fbb2ed0ec46a2d11ca133f4acfc0d
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-28 10:58:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-1028 Morning Meeting 10.00-10.35:
-Duàn:
-1. After enhancing local contrast, the effect improved. Find the reason, and check if there is any change in the peak diagram.
-2. The code and procedure need to be recorded. If bad version results are gone, record the best version's code and procedure.
-
-Hán:
-3. Focus on merging. Ensure the training image effect is good before continuing with this version's improvements.
-4. The code and procedure need to be recorded. If bad version results are gone, record the best version's code and procedure.
-5. When using training images for testing, organize the results and review them tomorrow.
-6. Add some kernel size and channels, then add Instance Normalization after each layer.
+Why enhancing contrast improves the effect?
