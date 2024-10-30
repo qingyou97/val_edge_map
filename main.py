e40842e9cec9874c82a18838300d549978655bf7
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-30 13:42:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-# 设置字体
-   font_path = "C:/Windows/Fonts/simsun.ttc"  # 这是Windows系统下的宋体路径
-   font_prop = font_manager.FontProperties(fname=font_path)
-   plt.rcParams['font.family'] = font_prop.get_name()
+  from matplotlib import font_manager
