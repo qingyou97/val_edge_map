c27a712fd230613d34ba712419308f7e74b58154
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-31 13:18:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-if slope is not None:
-        # 第一个点坐标
-        x1, y1 = line_points[0]
-        # 如果斜率是无穷大，意味着垂直线，我们直接构建与x平行的线
-        if np.isinf(slope):
-            extended_line = LineString([(x1, y) for y in range(40, 70)])  # 包含整个可能的y轴范围
-        else:
-            # 通过第一个点和斜率创建一个扩展的line_string
-            def y(x):
-                return y1 + slope * (x - x1)
-            x_coords = np.linspace(50, 70, 500)  # x范围可以适当调整
-            extended_line = LineString([(x, y(x)) for x in x_coords])
-    else:
-        # 如果有有效的两个点，正常扩展线
-        extended_line = LineString(line_points)
+October 31 Morning Meeting 1.00-1.17:
+
+Duan:
+1. When finding the peak value, it's necessary to use all the points for calculation, then find the points within the AI.
+2. After correcting issue 1 and other tolerances, test and compare the dataset.
+3. The best document should be reviewed tomorrow morning, including the branch, how to execute, how to reproduce, a separate Excel file, and all related work.
+
+Han:
+4. Try reducing the amount of merged input features.
+5. The best document should be reviewed tomorrow morning, including the branch, how to execute, how to reproduce, a separate Excel file, and all related work.
