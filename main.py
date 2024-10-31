daa0e25e3d99ef2d401ae9b9b6274d0890741c3b
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-31 14:12:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
- # 采样并检查所有在正方形内的点
-    all_points_inside = []
-    x_points = np.arange(x_min, x_max, step)
-    y_points = np.arange(y_min, y_max, step)
-
-    for x in x_points:
-        for y in y_points:
-            if polygon_path.contains_point((x, y)):
-                all_points_inside.append((x, y))
+x_min = min(rotated_points, key=lambda p: p[0])[0]
+    x_max = max(rotated_points, key=lambda p: p[0])[0]
+    y_min = min(rotated_points, key=lambda p: p[1])[1]
+    y_max = max(rotated_points, key=lambda p: p[1])[1]
