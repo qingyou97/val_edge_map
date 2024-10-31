ada4c1c36c24bdcd2f8a9357b131be7e1fc62578
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-10-31 15:34:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-找峰值时，是需要使用所有的点进行计算，再找ai内的点
+def sort_dict_by_value(data):
+    # 按照值从大到小排序，并转换为列表
+    sorted_list = sorted(data.items(), key=lambda item: item[1], reverse=True)
+    return sorted_list
+
+# 示例使用
+data = {1: 11, 2: 23, 3: 45}
+sorted_list = sort_dict_by_value(data)
+print(sorted_list)
