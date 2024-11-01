a7794f1c4d591d181c9c401f45aa1f6ad1cfb670
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-11-01 13:39:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def copy_folders_to_new_directory(source_dirs, destination_dir):
-    # 创建目标文件夹，如果不存在
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
-    
-    for folder in source_dirs:
-        # 获取文件夹名称
-        folder_name = os.path.basename(folder)
-        # 目标文件夹路径
-        destination_folder = os.path.join(destination_dir, folder_name)
-        # 复制文件夹
-        shutil.copytree(folder, destination_folder)
+1. Process the result of 8support using this algorithm: In each ai result's pixel, extend a line along the gradient direction of the point, then find the point of maximum peak on the extension line. For casting and bottle datasets, choose the pixel point closer to the circle center on the extension line. File link:
 
-source_dirs = ['/path/to/first/folder', '/path/to/second/folder', '/path/to/third/folder']
-destination_dir = '/path/to/new/directory'
+2. Process the result of 8support using this algorithm: For circular datasets, convert the sector to a rectangle for coordinate transformation. Use y-axis mapping to find the peak point, taking the maximum average in a 5-pixel length sliding window. For rectangular edge data, perform a sliding window on the rectangle, using size 5 for mapping and averaging. File link:
 
-copy_folders_to_new_directory(source_dirs, destination_dir)
+3. Process the 8support result using this algorithm: Traverse each point in the ai region, lock the point in place with a 10x10 square box, calculate the average gradient direction of all points within the square, rotate this box, then map and calculate the peak point in the rotated square, finally find a peak point within the ai region. File link:
