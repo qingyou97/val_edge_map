2658f7235855510b5595970f4eb09f0b34b9a888
qingyou <71199739+qingyou97@users.noreply.github.com>
2024-11-01 16:42:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-1. Organized deliverables and checked links:
-Conclusion: I placed all 77 deliverables in XXX's OneNote. Since my OneNote didn't update Han's content, I created a new OneNote with my surname. I verified all links, and they are clickable.
+6. The final best
 
-2. Tested 8 supports:
-Conclusion: Links as follows:
-
-3. Setup procedure and requirements.txt:
-Conclusion: Completed and located in the req.txt file under the rulebased-method branch.
-
-4. Branch info in the document:
-Conclusion: Recorded branch information in the best document.
-
-5. Chinese version materials:
-Conclusion: Translated all past and current deliverables into English.
+Previously, I compiled the best results for 4 support. This afternoon, I compiled the best results for 8 support.
